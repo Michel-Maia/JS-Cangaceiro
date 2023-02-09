af288fbdf550368930dba2eeb54eeaa7eb746fec
Michel Maia <programador.michel.maia@gmail.com>
2023-02-09 20:44:54 -0300

    commit

--- client/app/index.js
+++ client/app/index.js
@@ -1,9 +1,31 @@
-var campo = [ 
+var campos = [ 
     document.querySelector('#data'),
     document.querySelector('#valor'),
     document.querySelector('#quantidade')
 ];
 
-console.log(campo);
+//console.log(campos);
 
+var tbody = document.querySelector('table tbody');
+
+document.querySelector('form').addEventListener('submit', function(event) {
+
+    var tr = document.createElement('tr');
+
+    campos.forEach(function(campo) {
+
+        var td = document.createElement('td');
+        td.textContent = campo.value;
+        tr.appendChild(td);
+    });
+
+    var tdVolume = document.createElement('td');
+
+    tdVolume.textContent = campos[1].value * campos[2].value;
+    tr.appendChild(tdVolume);
+
+    tbody.appendChild(tr);
+});
+
+// pg 8
 
